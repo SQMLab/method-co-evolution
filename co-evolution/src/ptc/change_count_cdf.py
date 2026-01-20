640745dee8ac3d07825b10c431bc0e6272035458
Shahidul Islam <shahidul2k9@gmail.com>
2026-01-19 18:57:51 -0600

    format method change type columns

--- co-evolution/src/ptc/change_count_cdf.py
+++ co-evolution/src/ptc/change_count_cdf.py
@@ -3,12 +3,13 @@ import os
 from pathlib import Path
 import tarfile
 import json
+import re
 from ptc.constants import MethodChangeType
 cache_dir = os.environ.get("METHOD_CO_EVOLUTION_CACHE_DIRECTORY")
 
 
 for tooName in os.listdir(f"{cache_dir}/history"):
-    if not tooName.startswith("historyFinder"):
+    if tooName.startswith("historyFinder"):
         continue
     for zip_file in Path(f"{cache_dir}/history/{tooName}").rglob("*.tar.gz"):
         method_history_list = []
@@ -20,22 +21,23 @@ for tooName in os.listdir(f"{cache_dir}/history"):
                     file_content = tar.extractfile(file)
                     if file_content is not None:
                         history_json = json.load(file_content)
-                        change_commits = history_json["commits"]
+                        change_commits = history_json["changeHistoryShort"]
                         change_history = {ct.value: 0 for ct in MethodChangeType}
-                        change_history["all"] =  len(change_commits)
 
 
-                        for commit in change_commits:
-                            for change_type in commit["changeTags"]:
-                                change_history[change_type.lower()] += 1
-                        method_history = {"history_file" : base_file}
+                        # print(change_history)
+                        for commit_hash, change_text in change_commits.items():
+                            changes = [p.strip() for p in re.split(r'[(),]', change_text) if p.strip()]
+                            for change_type in changes:
+                                change_history[change_type] += 1
+                        method_history = {"history_file" : base_file, "ch_all": len(change_commits)}
                         for key, value in change_history.items():
-                            method_history[f"change_{key}"] = value
+                            method_history[f"ch_{MethodChangeType(key).name.lower()}"] = value
                             method_history_list.append(method_history)
         repository_change_history_file = f"{cache_dir}/data/history/{tooName}/{repository_name}--history.csv"
         os.makedirs(os.path.dirname(repository_change_history_file), exist_ok=True)
         pd.DataFrame(method_history_list).to_csv(repository_change_history_file, index=False)
-
+        break
 
 
 
