35fa2cea68f3100eefc95e2effd4cf8347ee01ce
Md. Nahidul Islam Opu <opu.nahidul@gmail.com>
2026-02-26 13:11:55 -0600

    integrate complexity analyzer

---
+++ method-history-collector/src/mhc/complexity_analyzer.py
@@ -0,0 +1,129 @@
+import os
+import pandas as pd
+import uuid
+import shutil
+import tarfile
+
+
+class ComplexityAnalyzer:
+    def __init__(
+        self,
+        cache_directory: str,
+        repository_directory: str,
+        data_directory: str,
+        repo_list: list[str],
+        jar_file_path: str,
+    ):
+        self.cache_directory = cache_directory
+        self.repository_directory = repository_directory
+        self.data_directory = data_directory
+        self.repo_list = repo_list
+        self.jar_file_path = jar_file_path
+
+        self.project_info_path = os.path.join(
+            self.data_directory,
+            "repository",
+            f"project_info_{str(uuid.uuid4())}.txt",
+        )
+        self.repository_df_path = os.path.join(
+            self.data_directory, "repository", "repository.csv"
+        )
+
+        self.history_json_dir = os.path.join(self.repository_directory, "historyJSON")
+        os.makedirs(self.history_json_dir, exist_ok=True)
+
+        self.output_dir = os.path.join(
+            self.data_directory, "complexity_analyzer_output"
+        )
+        os.makedirs(self.output_dir, exist_ok=True)
+
+        self.temp_dirs = []
+
+        self.create_project_info()
+        self.copy_extract_history()
+
+    def __del__(self):
+        self.delete_file(self.project_info_path)
+        for temp_dir in self.temp_dirs:
+            self.delete_directory(temp_dir)
+
+    def delete_file(self, file_path: str):
+        if os.path.exists(file_path):
+            os.remove(file_path)
+
+    def delete_directory(self, directory_path: str):
+        if os.path.isdir(directory_path):
+            shutil.rmtree(directory_path)
+
+    def create_project_info(self):
+        repo_stat = []
+
+        repository_df = pd.read_csv(self.repository_df_path)
+        repository_df = repository_df[repository_df["repo_name"].isin(self.repo_list)]
+
+        for _, row in repository_df.iterrows():
+            repo_stat.append(
+                {
+                    "name": row["repo_name"],
+                    "latest_commit_date": row["updated_at"],
+                    "latest_commit_hash": row["updated_hash"],
+                    "first_commit_date": row["created_at"],
+                    "first_commit_hash": row["created_hash"],
+                }
+            )
+
+        repo_stat_df = pd.DataFrame(repo_stat)
+        repo_stat_df.to_csv(self.project_info_path, index=False, header=False, sep="\t")
+
+    def copy_extract_structure_history_tar(self):
+        for repo_name in self.repo_list:
+            source_tar_path = os.path.join(
+                self.cache_directory, "history", "historyFinder", f"{repo_name}.tar.gz"
+            )
+
+            destination_tar_path = os.path.join(
+                self.history_json_dir, f"{repo_name}.tar.gz"
+            )
+
+            shutil.copy2(source_tar_path, destination_tar_path)
+
+            extracted_dir = os.path.join(
+                self.history_json_dir, repo_name + "_raw_history"
+            )
+            os.makedirs(extracted_dir, exist_ok=True)
+            with tarfile.open(destination_tar_path, "r:gz") as tar:
+                tar.extractall(path=extracted_dir)
+            self.delete_file(destination_tar_path)
+
+            restructured_dir = os.path.join(self.history_json_dir, repo_name)
+            os.makedirs(restructured_dir, exist_ok=True)
+            self.temp_dirs.append(restructured_dir)
+
+            file_number = 1
+            json_files = glob.glob(f"{extracted_dir}/**/*.json", recursive=True)
+            for file in json_files:
+                shutil.copy2(
+                    file, os.path.join(restructured_dir, f"{file_number}.json")
+                )
+                file_number += 1
+
+            self.delete_directory(extracted_dir)
+
+    def run_complexity_analyzer(self):
+        command = [
+            f"java -Xmx10240m -jar {self.jar_file_path} -projectsInfo {self.project_info_path} -codeShovelHistoryDir {self.history_json_dir} -resultDir {self.output_dir} -filterOutTestMethods false"
+        ]
+        try:
+            result = subprocess.run(
+                command,
+                capture_output=True,
+                text=True,
+                shell=True,
+                check=True,
+            )
+        except subprocess.CalledProcessError as e:
+            raise e
+        finally:
+            self.delete_file(self.project_info_path)
+            for temp_dir in self.temp_dirs:
+                self.delete_directory(temp_dir)
--- method-history-collector/src/mhc/main.py
+++ method-history-collector/src/mhc/main.py
@@ -6,29 +6,56 @@ from mhc.method_history_collector import *
 def main():
     parser = argparse.ArgumentParser(description="Method History Collector (MHC)")
 
-    parser.add_argument("command", type=str, help="Command to execute (e.g., history, call-graph)")
-    parser.add_argument("--cache_directory", type=str, required=True, help="Cache directory path")
-    parser.add_argument("--repository_directory", type=str, required=True, help="Repository directory path")
-    parser.add_argument("--data_directory", type=str, required=True, help="Data directory path")
-    parser.add_argument("--jar_directory", type=str, required=True, help="Jar directory path")
+    parser.add_argument(
+        "command", type=str, help="Command to execute (e.g., history, call-graph)"
+    )
+    parser.add_argument(
+        "--cache_directory", type=str, required=True, help="Cache directory path"
+    )
+    parser.add_argument(
+        "--repository_directory",
+        type=str,
+        required=True,
+        help="Repository directory path",
+    )
+    parser.add_argument(
+        "--data_directory", type=str, required=True, help="Data directory path"
+    )
+    parser.add_argument(
+        "--jar_directory", type=str, required=True, help="Jar directory path"
+    )
 
     # Conditional args for history command
-    parser.add_argument("--tool_name", type=str, help="Tool name (required for history command)")
-    parser.add_argument("--repository_name", type=str, help="Repository name (required for history command)")
+    parser.add_argument(
+        "--tool_name", type=str, help="Tool name (required for history command)"
+    )
+    parser.add_argument(
+        "--repository_name",
+        type=str,
+        help="Repository name (required for history command)",
+    )
 
     args = parser.parse_args()
 
-    mhc = MethodHistoryCollector(args.cache_directory, args.repository_directory, args.data_directory,
-                                 args.jar_directory)
+    mhc = MethodHistoryCollector(
+        args.cache_directory,
+        args.repository_directory,
+        args.data_directory,
+        args.jar_directory,
+    )
 
     if args.command.lower() == "history":
         if not args.tool_name or not args.repository_name:
-            print("Error: tool_name and repository_name are required for history command.")
+            print(
+                "Error: tool_name and repository_name are required for history command."
+            )
             sys.exit(1)
         mhc.collect_method_history([args.repository_name], [args.tool_name])
     elif args.command.lower() == "call-graph":
         if not args.tool_name or not args.repository_name:
-            print("Error: tool_name and repository_name are required for call graph command.")
+            print(
+                "Error: tool_name and repository_name are required for call graph command."
+            )
             sys.exit(1)
         mhc.generate_call_graph([args.repository_name], [args.tool_name])
     elif args.command.lower() == "scan-method":
@@ -38,6 +65,13 @@ def main():
         mhc.scan_method([args.repository_name])
     elif args.command.lower() == "index":
         mhc.update_repository_index()
+    elif args.command.lower() == "complexity-analyzer":
+        if not args.tool_name or not args.repository_name:
+            print(
+                "Error: tool_name and repository_name are required for complexity analyzer command."
+            )
+            sys.exit(1)
+        mhc.run_complexity_analyzer([args.repository_name], args.tool_name)
     else:
         print(f"Unknown command: {args.command}")
         sys.exit(1)
