ca9e90909cabc337ea55e72c67014f041c873937
shahidul <shahidul2k9@gmail.com>
2026-03-02 05:51:59 -0600

    improve test vs test util identification logic

--- method-parser/src/test/java/MethodGenerationTest.java
+++ method-parser/src/test/java/MethodGenerationTest.java
@@ -7,9 +7,13 @@ import rnd.method.parser.call.graph.model.Method;
 import rnd.method.parser.call.graph.service.MethodScannerImpl;
 import rnd.method.parser.call.graph.util.TableUtil;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author Shahidul Islam
@@ -32,12 +36,39 @@ public class MethodGenerationTest extends TestConfigurationBase {
                 configurations.stream().map(projectConfig -> DynamicContainer.dynamicContainer(projectConfig.name,
                         projectConfig.cases.stream().map(testCase -> DynamicTest.dynamicTest(testCase.name, () -> {
 
+
                             MethodScannerImpl methodScanner = new MethodScannerImpl();
-                            List<Method> methods = methodScanner.scanMethod(TestConfigurationBase.resolvePlaceholders(projectConfig.repositoryPath), projectConfig.repositoryUrl,
-                                    projectConfig.commitHash, testCase.targetPath);
 
+                            String repoRoot = TestConfigurationBase.resolvePlaceholders(projectConfig.repositoryPath);
+                            Path repoRootPath = Paths.get(repoRoot).toAbsolutePath().normalize();
+                            Path targetPath = repoRootPath.resolve(testCase.targetPath).normalize();
+
+                            List<Method> methods;
+                            if (Files.isRegularFile(targetPath) && targetPath.toString().endsWith(".java")) {
+                                String relativePath = repoRootPath.relativize(targetPath).toString();
+
+                                methods = methodScanner.scanMethod(
+                                        repoRoot,
+                                        projectConfig.repositoryUrl,
+                                        projectConfig.commitHash,
+                                        relativePath
+                                );
+                            } else {
+                                try (Stream<Path> pathStream = Files.walk(targetPath)) {
+                                    methods = pathStream
+                                            .filter(Files::isRegularFile)
+                                            .filter(path -> path.toString().endsWith(".java"))
+                                            .map(path -> repoRootPath.relativize(path).toString())
+                                            .flatMap(relativePath -> methodScanner.scanMethod(
+                                                    repoRoot,
+                                                    projectConfig.repositoryUrl,
+                                                    projectConfig.commitHash,
+                                                    relativePath
+                                            ).stream())
+                                            .collect(Collectors.toList());
+                                }
+                            }
                             String outputDirectory = TestConfigurationBase.resolvePlaceholders(testCase.outputDirectory);
-
                             TableUtil.toTable(methods, String.format(Locale.CANADA, "%s/method/%s/%s--%s.csv", outputDirectory, projectConfig.name, testCase.name, projectConfig.commitHash));
 
                         })))));
