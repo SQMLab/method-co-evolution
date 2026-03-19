a82a56f484e2686fc6b44397d34ba4c98e3a4c66
shahidul <shahidul2k9@gmail.com>
2026-03-19 11:46:51 -0500

    move out inner method call expansion

--- co-evolution/src/ptc/generator/generate_m2m_tech.py
+++ co-evolution/src/ptc/generator/generate_m2m_tech.py
@@ -16,10 +16,10 @@ MAX_EXPANSION_DEPTH = 5
 
 FANOUT_DIR = f"{DATA_DIRECTORY}/fan-out"
 METHOD_DIR = f"{DATA_DIRECTORY}/method"
-EXPANDED_FANOUT_DIR = f"{DATA_DIRECTORY}/t2p-expanded-fan-out"
+T2P_CANDIDATE_DIR = f"{DATA_DIRECTORY}/t2p-candidate"
 OUTPUT_DIR = f"{DATA_DIRECTORY}/m2m-tech"
 
-os.makedirs(EXPANDED_FANOUT_DIR, exist_ok=True)
+os.makedirs(T2P_CANDIDATE_DIR, exist_ok=True)
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 # ---------------------------
@@ -52,43 +52,6 @@ def establish_confidence(row):
     })
 
 
-
-def expand_test_calls(row, from_url_graph, method_artifact_mapping, max_depth):
-    results = []
-
-    stack = deque()
-    visited = set()
-
-    stack.append((row, 1, row["to_url"]))
-
-    while stack:
-        current_row, depth, current_to_url = stack.pop()
-
-        if depth <= max_depth and current_to_url not in visited:
-            visited.add(current_to_url)
-
-            artifact = method_artifact_mapping.get(current_to_url, "")
-
-            # Stop if production reached
-            if artifact == "production":
-                new_row = row.copy()
-
-                for col in current_row.index:
-                    if col.startswith("to_"):
-                        new_row[col] = current_row[col]
-
-                new_row["to_caller_url"] = current_row["from_url"]
-                new_row["to_call_depth"] = depth
-
-                results.append(new_row)
-            else:
-                # otherwise expand
-                for next_row in from_url_graph.get(current_to_url, []):
-                    stack.append((next_row, depth + 1, next_row["to_url"]))
-
-    return results
-
-
 # ---------------------------
 # Main Processing
 # ---------------------------
@@ -98,73 +61,37 @@ for _, repo in repository_df.iterrows():
     project = repo["project"]
     commit_hash = repo["updated_hash"]
 
-    fanout_file = f"{FANOUT_DIR}/{project}.csv"
+    t2p_candidate_file = f"{T2P_CANDIDATE_DIR}/{project}.csv"
     method_file = f"{METHOD_DIR}/{project}.csv"
 
-    if os.path.exists(fanout_file) and os.path.exists(method_file):
+    if os.path.exists(t2p_candidate_file):
         print("Processing:", project)
 
-        fan_out_df = pd.read_csv(fanout_file, na_filter=False, keep_default_na=False)
-        method_df = pd.read_csv(method_file, na_filter=False, keep_default_na=False)
-
-        fan_out_df = fan_out_df[fan_out_df["to_url"].str.strip() != ""]
-
-        # Build artifact lookup
-        method_artifact = dict(zip(method_df["url"], method_df["artifact"]))
-
-        # Build call graph
-        from_url_graph = defaultdict(list)
-
-        for _, row in fan_out_df.iterrows():
-            from_url_graph[row["from_url"]].append(row)
-
-        expanded_rows = []
-
-        for _, row in fan_out_df.iterrows():
-
-            from_artifact = method_artifact.get(row["from_url"], "")
-            to_artifact = method_artifact.get(row["to_url"], "")
-
-            # only expand test → test/test util
-            if from_artifact == "test" and (to_artifact == "test" or to_artifact == "test_util"):
-                expansions = expand_test_calls(
-                    row,
-                    from_url_graph,
-                    method_artifact,
-                    MAX_EXPANSION_DEPTH
-                )
-                expanded_rows.extend(expansions)
-            else:
-                expanded_rows.append(row)
-
-        expanded_df = pd.DataFrame(expanded_rows)
-
-        expanded_file = f"{EXPANDED_FANOUT_DIR}/{project}.csv"
-        expanded_df.to_csv(expanded_file, index=False)
+        t2p_candidate_df = pd.read_csv(t2p_candidate_file, na_filter=False, keep_default_na=False)
 
         # ---------------------------
         # Apply Techniques
         # ---------------------------
 
-        expanded_df[[
+        t2p_candidate_df[[
             "tech_nc",
             "tech_ncc",
             "tech_lcs_b",
             "tech_lcs_u",
             "tech_leven"
-        ]] = expanded_df.apply(
+        ]] = t2p_candidate_df.apply(
             establish_confidence,
             axis=1
         ).round(2)
 
-        expanded_df["tech_lc"] = (
-                expanded_df.groupby("from_url").cumcount()
-                == expanded_df.groupby("from_url")["from_url"].transform("size") - 1
+        t2p_candidate_df["tech_lc"] = (
+                t2p_candidate_df.groupby("from_url").cumcount()
+                == t2p_candidate_df.groupby("from_url")["from_url"].transform("size") - 1
         ).astype(int)
 
-        expanded_df["tech_lcba"] = expanded_df["to_lcba"].astype(int)
+        t2p_candidate_df["tech_lcba"] = t2p_candidate_df["to_lcba"].astype(int)
 
-        expanded_df = util.convert_float_int_columns_to_nullable_int(expanded_df)
+        expanded_df = util.convert_float_int_columns_to_nullable_int(t2p_candidate_df)
 
         output_file = f"{OUTPUT_DIR}/{project}.csv"
         expanded_df.to_csv(output_file, index=False)
