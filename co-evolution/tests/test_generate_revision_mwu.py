ead1c4dd460cb6a94ab8ad2896051d50a18c6d00
shahidul <shahidul2k9@gmail.com>
2026-05-24 08:35:33 -0500

    add artifact prefix

--- co-evolution/tests/test_generate_revision_mwu.py
+++ co-evolution/tests/test_generate_revision_mwu.py
@@ -16,7 +16,7 @@ except ImportError:  # pragma: no cover
     pd = None
 
 from ptc.constants import ALL_REPOSITORY
-from ptc.generator.revision_mww import main
+from ptc.generator.artifact_revision_mww import main
 
 
 @unittest.skipIf(pd is None, "pandas is required for generate_revision_mwu tests")
